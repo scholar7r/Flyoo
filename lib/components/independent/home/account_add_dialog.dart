b642431b8228c1f9c2d1b65aa20bc82f6e553f62
scholar7r <scholar7r@outlook.com>
2025-04-02 22:34:47 +0800

    refactor(dialogs): move to alert dialog

--- lib/components/independent/home/account_add_dialog.dart
+++ lib/components/independent/home/account_add_dialog.dart
@@ -23,119 +23,100 @@ class _AccountAddDialogState extends State<AccountAddDialog> {
   Widget build(BuildContext context) {
     final accountProvider = Provider.of<AccountProvider>(context);
 
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(18),
-        child: ListView(
-          shrinkWrap: true,
+    return AlertDialog(
+      title: Text(AppLocalizations.of(context)!.addAccount),
+      content: SingleChildScrollView(
+        child: Column(
+          spacing: 12,
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: Text(
-                AppLocalizations.of(context)!.addAccount,
-                style: TextStyle(fontWeight: FontWeight.w500),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(AppLocalizations.of(context)!.alias),
+                errorText: _accountAliasErrorText,
               ),
+              controller: _accountAliasController,
             ),
 
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text(AppLocalizations.of(context)!.alias),
-                  errorText: _accountAliasErrorText,
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(AppLocalizations.of(context)!.userName),
+                errorText: _accountNameErrorText,
+              ),
+              controller: _accountNameController,
+              keyboardType: TextInputType.phone,
+            ),
+
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                label: Text(AppLocalizations.of(context)!.password),
+                errorText: _accountPassErrorText,
+              ),
+              obscureText: true,
+              controller: _accountPassController,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              spacing: 8,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text(AppLocalizations.of(context)!.cancel),
                 ),
-                controller: _accountAliasController,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text(AppLocalizations.of(context)!.userName),
-                  errorText: _accountNameErrorText,
+            
+                ElevatedButton(
+                  onPressed: () async {
+                    /////////////////////Validation///////////////////////////
+                    setState(() {
+                      _accountAliasErrorText =
+                          _accountAliasController.text.isEmpty
+                              ? AppLocalizations.of(context)!.emptyAlias
+                              : null;
+                      _accountNameErrorText =
+                          RegExp(
+                                r'^1[3-9]\d{9}$',
+                              ).hasMatch(_accountNameController.text)
+                              ? null
+                              : AppLocalizations.of(
+                                context,
+                              )!.notRegularPhoneNumber;
+                      _accountPassErrorText =
+                          _accountPassController.text.isEmpty
+                              ? AppLocalizations.of(context)!.emptyPassword
+                              : null;
+                    });
+            
+                    if (_accountAliasErrorText != null ||
+                        _accountNameErrorText != null ||
+                        _accountPassErrorText != null) {
+                      return;
+                    }
+            
+                    // int effectedLines = await DatabaseHelper().insertAccount(
+                    //   Account(
+                    //     accountAlias: _accountAliasController.text,
+                    //     accountName: _accountNameController.text,
+                    //     accountPass: md5Digest(_accountPassController.text),
+                    //   ),
+                    // );
+                    await accountProvider.insertAccount(
+                      Account(
+                        accountAlias: _accountAliasController.text,
+                        accountName: _accountNameController.text,
+                        accountPass: _accountPassController.text,
+                      ),
+                    );
+            
+                    if (!context.mounted) return;
+                    Navigator.pop(context);
+                  },
+                  child: Text(AppLocalizations.of(context)!.add),
                 ),
-                controller: _accountNameController,
-                keyboardType: TextInputType.phone,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  label: Text(AppLocalizations.of(context)!.password),
-                  errorText: _accountPassErrorText,
-                ),
-                obscureText: true,
-                controller: _accountPassController,
-              ),
-            ),
-
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                spacing: 8,
-                children: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text(AppLocalizations.of(context)!.cancel),
-                  ),
-
-                  ElevatedButton(
-                    onPressed: () async {
-                      /////////////////////Validation///////////////////////////
-                      setState(() {
-                        _accountAliasErrorText =
-                            _accountAliasController.text.isEmpty
-                                ? AppLocalizations.of(context)!.emptyAlias
-                                : null;
-                        _accountNameErrorText =
-                            RegExp(
-                                  r'^1[3-9]\d{9}$',
-                                ).hasMatch(_accountNameController.text)
-                                ? null
-                                : AppLocalizations.of(
-                                  context,
-                                )!.notRegularPhoneNumber;
-                        _accountPassErrorText =
-                            _accountPassController.text.isEmpty
-                                ? AppLocalizations.of(context)!.emptyPassword
-                                : null;
-                      });
-
-                      if (_accountAliasErrorText != null ||
-                          _accountNameErrorText != null ||
-                          _accountPassErrorText != null) {
-                        return;
-                      }
-
-                      // int effectedLines = await DatabaseHelper().insertAccount(
-                      //   Account(
-                      //     accountAlias: _accountAliasController.text,
-                      //     accountName: _accountNameController.text,
-                      //     accountPass: md5Digest(_accountPassController.text),
-                      //   ),
-                      // );
-                      await accountProvider.insertAccount(
-                        Account(
-                          accountAlias: _accountAliasController.text,
-                          accountName: _accountNameController.text,
-                          accountPass: _accountPassController.text,
-                        ),
-                      );
-
-                      if (!context.mounted) return;
-                      Navigator.pop(context);
-                    },
-                    child: Text(AppLocalizations.of(context)!.add),
-                  ),
-                ],
-              ),
+              ],
             ),
           ],
         ),
--- lib/components/independent/home/account_edit_dialog.dart
+++ lib/components/independent/home/account_edit_dialog.dart
@@ -15,6 +15,52 @@ class _AccountEditDialogState extends State<AccountEditDialog> {
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(AppLocalizations.of(context)!.edit),
+      content: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 12,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                label: Text(AppLocalizations.of(context)!.alias),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            TextField(
+              decoration: InputDecoration(
+                label: Text(AppLocalizations.of(context)!.userName),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            TextField(
+              decoration: InputDecoration(
+                label: Text(AppLocalizations.of(context)!.password),
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            Row(
+              spacing: 8,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(AppLocalizations.of(context)!.cancel),
+                ),
+
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(AppLocalizations.of(context)!.save),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
